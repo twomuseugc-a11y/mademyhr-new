188af2430b2992926a85e1cc6b5ea3ecec6e531b
ruthwikss <leadurlifepeace@gmail.com>
2026-04-14 18:02:05 +0530

    fix layout children type

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { CartProvider } from "./context/CartContext";
+import type { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
